05eb2b4965469136daa05554be9bb0a8c9c59180
liang <lianghao@whu.edu.cn>
2023-08-04 12:57:46 +0800

    solve the bug about validation

--- source/train.py
+++ source/train.py
@@ -13,7 +13,7 @@ from source.utils.common.log_util import Recorder, Logger
 
 
 def train(model, recoder, logger, main_flag):
-    val_sum = len(model.test_loader)
+    val_sum = model.val_sum
     cur_iter = 0
     best_psnr = {
         "iter": 0,
@@ -29,34 +29,34 @@ def train(model, recoder, logger, main_flag):
                 f"epoch [{epoch} / {model.end_epoch}]")
             loop_train.set_postfix(lr=model.optimizer.param_groups[0]['lr'])
 
-            if cur_iter % model.val_freq == 0 and main_flag:
+            if cur_iter % model.val_freq == 0:
                 # 不进行分布式验证，在一个gpu上完成验证
                 model.net_g.eval()
                 res_metric = {}
                 with torch.no_grad():
                     for _, val_data in enumerate(model.test_loader):
-                        res = model.__eval__(data)
+                        res = model.__eval__(val_data)
                         # update
                         for key in res.keys():
                             res_metric[key] = res_metric.get(key, 0) + res[key]
+                    if main_flag:
+                        for key in res_metric.keys():
+                            res_metric[key] = res_metric[key] / val_sum
 
-                    for key in res_metric.keys():
-                        res_metric[key] = res_metric[key] / val_sum
+                        if res_metric["psnr"] > best_psnr["psnr"]:
+                            best_psnr["psnr"] = res_metric["psnr"]
+                            best_psnr["iter"] = cur_iter
 
-                    if res_metric["psnr"] > best_psnr["psnr"]:
-                        best_psnr["psnr"] = res_metric["psnr"]
-                        best_psnr["iter"] = cur_iter
-
-                    # 打印信息
-                    info = f"epoch : {epoch}, cur_lr : {model.optimizer.param_groups[0]['lr'] : .6f} \n" + ' ' * 32
-                    for key in res_metric.keys():
-                        info += f"{key} : {res_metric[key] : .6f}    "
-                    info += '\n' + ' ' * 32 + f"best psnr : {best_psnr['psnr'] : .6f} @ iter {best_psnr['iter']}"
-                    logger.info(info)
-                    # 保存
-                    net_warp = model.accelerator.unwrap_model(model.net_g)
-                    best_path = os.path.join(recoder.sub_dirs['best_ckpt'], f'best_iter_{cur_iter}.ckpt')
-                    torch.save({'net': net_warp.state_dict()}, best_path)
+                        # 打印信息
+                        info = f"epoch : {epoch}, cur_lr : {model.optimizer.param_groups[0]['lr'] : .6f} \n" + ' ' * 32
+                        for key in res_metric.keys():
+                            info += f"{key} : {res_metric[key] : .6f}    "
+                        info += '\n' + ' ' * 32 + f"best psnr : {best_psnr['psnr'] : .6f} @ iter {best_psnr['iter']}"
+                        logger.info(info)
+                        # 保存
+                        net_warp = model.accelerator.unwrap_model(model.net_g)
+                        best_path = os.path.join(recoder.sub_dirs['best_ckpt'], f'best_iter_{cur_iter}.ckpt')
+                        torch.save({'net': net_warp.state_dict()}, best_path)
 
 
             if cur_iter % model.save_freq == 0 and main_flag:
