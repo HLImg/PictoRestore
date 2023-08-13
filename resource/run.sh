78055453bae8e67d7df657570fbf89d5e6268beb
liang <lianghao@whu.edu.cn>
2023-08-13 18:09:04 +0800

    update：script
    Signed-off-by: liang <lianghao@whu.edu.cn>

--- resource/run.sh
+++ resource/run.sh
@@ -1,1 +1,4 @@
+# git push to github
+git push --force origin master
+# run training script
 accelerate launch --config_file=resource/acc.yaml --machine_rank=0 --num_machines=1 --main_process_port=29506 main.py  --yaml options/nafnet/train_nafnet_wf_32.yaml
