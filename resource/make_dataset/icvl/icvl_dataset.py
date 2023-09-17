cc3cf01736482068993c2832d2cb69b65287b17a
Hao Liang <lianghao@whu.edu.cn>
2023-09-17 16:07:52 +0800

    update test

--- resource/make_dataset/icvl/icvl_dataset.py
+++ resource/make_dataset/icvl/icvl_dataset.py
@@ -14,6 +14,7 @@ from tqdm import tqdm
 from scipy.io import savemat, loadmat
 from down_icvl_hsi import load_hsi
 
+
 def get_total_file_buffer_size(paths, is_h5py=False):
     total_size = 0
     for i in tqdm(range(len(paths)), desc='buffer'):
@@ -79,6 +80,30 @@ def crop_hsi_data(paths, save_dir, patch_size=256, stride=32, is_h5py=True):
     print(f'the number of patches is {count}')
 
 
+def crop_hsi_test_data(paths, save_dir, patch_size=512, is_hypy=True):
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    count = 0
+    for i in tqdm(range(len(paths)), desc='test-croping'):
+        path = paths[i]
+        _, file = os.path.split(path)
+        name, _ = os.path.splitext(file)
+        data = load_hsi(path, is_h5py=is_hypy)['rad']
+        
+        data = np.rot90(data, k=-1, axes=(1, 2))
+        c, h, w = data.shape
+        ind_h = h // 2 - (patch_size // 2)
+        ind_w = w // 2 - (patch_size // 2)
+        
+        patch = data[:, ind_h: ind_h + patch_size, ind_w: ind_w + patch_size]
+        savemat(file_name=os.path.join(save_dir, f'{name}_center.mat'), mdict={'rad': patch})
+        count += 1
+    
+    print('*************************************************************')
+    print(f'the number of patches is {count}')
+    
+                
+
 
 if __name__ == '__main__':
     meta_dir = './meta_info'
@@ -90,15 +115,17 @@ if __name__ == '__main__':
         'test': '/data/dataset/HSI/ICVL/icvl_test.lmdb'
     }
 
-    train_paths = get_path(icvl_dir, meta_dir, 'train')
-    crop_hsi_data(train_paths, save_dir='/data/dataset/HSI/ICVL/icvl_train', patch_size=256, stride=192)
-    train_paths = [path for path in glob.glob('/data/dataset/HSI/ICVL/icvl_train/*.mat')]
-    make_lmdb(train_paths, lmdb_path=dataset['train'], is_h5py=False)
+    # train_paths = get_path(icvl_dir, meta_dir, 'train')
+    # crop_hsi_data(train_paths, save_dir='/data/dataset/HSI/ICVL/icvl_train', patch_size=256, stride=192)
+    # train_paths = [path for path in glob.glob('/data/dataset/HSI/ICVL/icvl_train/*.mat')]
+    # make_lmdb(train_paths, lmdb_path=dataset['train'], is_h5py=False)
 
-    valid_paths = get_path(icvl_dir, meta_dir, 'valid')
-    crop_hsi_data(valid_paths, save_dir='/data/dataset/HSI/ICVL/icvl_valid', patch_size=512, stride=512)
-    valid_paths = [path for path in glob.glob('/data/dataset/HSI/ICVL/icvl_valid/*.mat')]
-    make_lmdb(valid_paths, lmdb_path=dataset['valid'], is_h5py=False)
+    # valid_paths = get_path(icvl_dir, meta_dir, 'valid')
+    # crop_hsi_data(valid_paths, save_dir='/data/dataset/HSI/ICVL/icvl_valid', patch_size=512, stride=512)
+    # valid_paths = [path for path in glob.glob('/data/dataset/HSI/ICVL/icvl_valid/*.mat')]
+    # make_lmdb(valid_paths, lmdb_path=dataset['valid'], is_h5py=False)
 
-    # for mode, lmdb_path in dataset.items():
-    #     make_lmdb(icvl_dir, meta_dir=meta_dir, mode=mode, lmdb_path=lmdb_path)
+    test_paths = get_path(icvl_dir, meta_dir, 'test')
+    crop_hsi_test_data(test_paths, save_dir='/data/dataset/HSI/ICVL/icvl_test', patch_size=512)
+    test_paths = [path for path in glob.glob('/data/dataset/HSI/ICVL/icvl_test/*.mat')]
+    make_lmdb(test_paths, lmdb_path=dataset['test'], is_h5py=False)
