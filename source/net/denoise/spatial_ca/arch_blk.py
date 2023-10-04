30c5972c99dd647b28397d6f63dbc56f36bd184e
Hao Liang <lianghao@whu.edu.cn>
2023-10-04 16:16:29 +0800

    update

--- source/net/denoise/spatial_ca/arch_blk.py
+++ source/net/denoise/spatial_ca/arch_blk.py
@@ -10,12 +10,15 @@ from .arch_spatial import NonLocalTransformer
 from source.net.denoise.nafnet.net_utils import NAFBlock
 
 class NonLocalCAV2(nn.Module):
-    def __init__(self, in_ch, sa_expand, ffn_expand, ksizes, dropout_rate=0.0, group=True, act_name_sa='relu', act_name_mlp='relu', bias=True):
+    def __init__(self, in_ch, sa_expand_sp, ffn_expand_sp, 
+                 sa_expand_ch=2, ffn_expand_ch=2,
+                 ksizes=[3, 3, 3, 3, 3], dropout_rate=0.0, 
+                 group=True, act_name_sa='relu', act_name_mlp='relu', bias=True):
         super(NonLocalCAV2, self).__init__()
         
-        self.sa_block = NonLocalTransformer(in_ch, sa_expand, ffn_expand, ksizes, dropout_rate, 
+        self.sa_block = NonLocalTransformer(in_ch, sa_expand_sp, ffn_expand_sp, ksizes, dropout_rate, 
                                             group, act_name_sa, act_name_mlp, bias)
-        self.ca_block = NAFBlock(in_ch, sa_expand, ffn_expand, dropout_rate)
+        self.ca_block = NAFBlock(in_ch, sa_expand_ch, ffn_expand_ch, dropout_rate)
         
     def forward(self, x):
         x = self.sa_block(x)
