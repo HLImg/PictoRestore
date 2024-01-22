c0dec3798e0dc0ed17ad67abc8445e5bbad0735e
lianghao02 <lianghao02@megvii.com>
2024-01-22 23:15:23 +0800

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-accelerate launch --config_file=tools/single_acc.yml --num_processes=8 main.py --config config/sidd_nafnet_wf32.yml --eval_ddp True --verbose True --train 
+# accelerate launch --config_file=tools/single_acc.yml --num_processes=8 main.py --config config/sidd_nafnet_wf32.yml --eval_ddp True --verbose True --train 
+
+accelerate launch --config_file=tools/single_acc.yml --num_processes=8 main.py --config config/psct_wf_32.yml --eval_ddp True --verbose True --train 
