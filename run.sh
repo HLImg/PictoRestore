930adb9da1d392894f79c303b8adb8d6e5d729c5
lianghao02 <lianghao02@megvii.com>
2024-01-26 12:13:53 +0800

    update

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@
 
 # accelerate launch --config_file=tools/single_acc.yml --num_processes=1 main.py --config config/pretrain_vdn.yml --eval_ddp False --verbose True --train 
 
-accelerate launch --config_file=tools/single_acc.yml --num_processes=8 main.py --config config/pretrain_vem.yml --eval_ddp False --verbose True --resume True --resume_state results/VEModelPretrain-SSCT/save_state/save_state_225000 --train 
+accelerate launch --config_file=tools/single_acc.yml --num_processes=8 main.py --config config/pretrain_vem.yml --eval_ddp False --verbose True --train 
